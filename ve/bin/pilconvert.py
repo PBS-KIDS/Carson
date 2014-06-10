aff926cd46ad6634fe69ae93866b255a4d84bda4
Sam Deng <sdeng@pbs.org>
2014-06-10 13:11:11 -0400

    Removed uncessary files.

--- ve/bin/pilconvert.py
+++
@@ -1,5 +0,0 @@
-#!/Users/blink/work/carson/bates/ve/bin/python
-# EASY-INSTALL-SCRIPT: 'PIL==1.1.7','pilconvert.py'
-__requires__ = 'PIL==1.1.7'
-import pkg_resources
-pkg_resources.run_script('PIL==1.1.7', 'pilconvert.py')
